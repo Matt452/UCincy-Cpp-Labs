07ba5de519d79c3b90e2d31763273ad70ad50879
Matt452 <123432996+Matt452@users.noreply.github.com>
2024-11-26 13:53:23 -0500

    Add files via upload

---
+++ InItToWinIt.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+//John Lennon, Paul McCartney, George Harrison, Ringo Starr
+/*
+The purpose of this program is to display
+my competition entry contianing: The instrument used,
+the name of the song, the preformers,
+and the time it took to preform the song.
+*/
+//Writing Credit: George Harrison
+int main() {
+	std::cout << "Air Guitar" << std::endl;
+	std::cout << "Here Comes the Sun\n";
+	std::cout << "As performed by: The Beatles" << std::endl;
+	std::cout << "Time: 3:06" << std::endl;
+	return 0;
+}
